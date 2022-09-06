a4b17d68d68ee04eb1fd22ca54dd327c385c7629
Krishnapal <krishnapals@tech-radix.com>
2022-09-06 20:05:22 +0530

    final commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'our-english';
+
+  @ViewChild('videoEl', { static: true }) private videoElRef: any;
+  @ViewChild('videoContainerEl', { static: true }) private videoContainerElRef: any;
+
+  get video(): HTMLVideoElement {
+    return this.videoElRef.nativeElement;
+  }
+
+  get videoContainer(): HTMLElement {
+    return this.videoElRef.nativeElement;
+  }
+
+
+  onPlay() {
+    console.log("onPlay");
+    this.video.play();
+  }
+
+  onPause() {
+    this.video.pause();
+  }
+
 }
